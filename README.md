add7a96b75c47913ab41f7f755fd5f0c087aa738
GitPit <rifflifter@gmail.com>
2021-07-20 18:52:35 -0400

    changed the color code to vanilla js

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # in-the-red
 
-This is a daily scheduler based on a 9-5 eight hour work day.
+This is a daily scheduler based on a 9-5 eight hour work day.  The time blocks turn grey when they are in the past, green for the future, and red for the present hour.
 
 ## About
 
-This is my first project using moment.js, Bootstrap, and JQuery.
-The scheduler contains JavaScript and Jquery functions utilizes local storage to save the day's events.
-
-### Usage
+This is my first project using moment.js, Bootstrap, and JQuery. I also incorporated some vanilla JavaScript.
+The application utilizes local storage to save the day's events.
 
 
-#### License
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,38 @@
-
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-
 function getSaved(value) {
     var savedNine = localStorage.getItem('9am');
-    document.getElementById("9am").innerHTML = savedNine;  
+    document.getElementById("9am").innerHTML = savedNine;
     console.log(savedNine);
 
     var savedTen = localStorage.getItem('10am');
-    document.getElementById("10am").innerHTML = savedTen;  
+    document.getElementById("10am").innerHTML = savedTen;
     console.log(savedTen);
 
     var savedEleven = localStorage.getItem('11am');
-    document.getElementById("11am").innerHTML = savedEleven;  
+    document.getElementById("11am").innerHTML = savedEleven;
     console.log(savedEleven);
 
     var savedTwelve = localStorage.getItem('12pm');
-    document.getElementById("12pm").innerHTML = savedTwelve;  
+    document.getElementById("12pm").innerHTML = savedTwelve;
     console.log(savedTwelve);
 
     var savedOne = localStorage.getItem('1pm');
-    document.getElementById("1pm").innerHTML = savedOne;  
+    document.getElementById("1pm").innerHTML = savedOne;
     console.log(savedOne);
 
     var savedTwo = localStorage.getItem('2pm');
-    document.getElementById("2pm").innerHTML = savedTwo;  
+    document.getElementById("2pm").innerHTML = savedTwo;
     console.log(savedTwo);
 
     var savedThree = localStorage.getItem('3pm');
-    document.getElementById("3pm").innerHTML = savedThree;  
+    document.getElementById("3pm").innerHTML = savedThree;
     console.log(savedThree);
 
     var savedFour = localStorage.getItem('4pm');
-    document.getElementById("4pm").innerHTML = savedFour;  
+    document.getElementById("4pm").innerHTML = savedFour;
     console.log(savedFour);
 
     var savedFive = localStorage.getItem('5pm');
-    document.getElementById("5pm").innerHTML = savedFive;  
+    document.getElementById("5pm").innerHTML = savedFive;
     console.log(savedFive);
 };
 
@@ -57,24 +50,10 @@ $("#currentDay").text(currentDay);
 console.log(currentDay);
 
 
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-
-
-
-
-
 // local storage
 
 // after a user types in box, they click this button
-$(".saveBtn").on("click", function() {
+$(".saveBtn").on("click", function () {
     // ** reach into the html, grab related input box text, save it to variable
     // getht he button that was clicked
     var theButton = $(this);
@@ -94,141 +73,62 @@ $(".saveBtn").on("click", function() {
     console.log(textContent);
     // --- save text input to local storage ---
 
-    
-    
-    
+
     // convert that data we just grabbed into a json string
     localStorage.setItem(idOfTextArea, textContent);
     // set it to local storage
 
-    
-    
-   
-    
-    
-        
-
-
 
     // test it
 
-    
+
     // console.log(value);
 });
 
 getSaved();
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
 //color code
 
 
 function colorCode() {
 
-    var hour = time.hours();
-    
-    $(".description").each(function () {
-        
-        var thisHour = parseInt($(this).attr("alt"));
-        if (thisHour > hour) {
-            $(this).addClass("future")
-        }
-        else if (thisHour === hour) {
-            $(this).addClass("present");
-        }
-        else {
-            $(this).addClass("past");
-        }
-    })
+    // define hour 
 
+    var currentHour = time.hours();
+    console.log(currentHour);
+
+    // target time blocks
+
+    // reach into the html and grab all of the time boxes
+    document.querySelectorAll('.description')
+        .forEach(function(eachElement) {
+            // for loop over each hour
+
+            console.dir(eachElement);
+
+            var hourOfElementAsString = eachElement.attributes.alt.nodeValue;
+            var hourOfElement = parseInt(hourOfElementAsString);
+            console.log(hourOfElement)
+            
+            // compare hourOfElement to current hour and apply the appropriate class
+    
+    
+    
+            if (hourOfElement > currentHour) {
+                // add the class to the element
+                eachElement.classList.add("future")
+            } else if (hourOfElement === currentHour) {
+                eachElement.classList.add("present")
+            } else {
+                eachElement.classList.add("past")
+            } 
+        })
 };
 
 
-// var ten = moment(10).format("HH");
-// var eleven = moment(11).format("HH");
-// var twelve = moment(12).format("HH");
-// var one = moment(13).format("HH");
-// var two = moment(14).format("HH");
-// var three = moment(15).format("HH");
-// var four = moment(16).format("HH");
-// var five = moment(17).format("HH");
-// var six = moment(18).format("HH");
-
-
-//     if (time < nine) {
-//         $("#9am").addClass("future");
-//     } else if (nine < time < ten) {
-//         $("#9am").removeClass("future").addClass("present");
-//     } else {
-//         $("#9am").removeClass("present").addClass("past");
-//     };
-
-
-//     if (time < ten) {
-//         $("#10am").addClass("future");
-//     } else if (ten < time < eleven) {
-//         $("#10am").removeClass("future").addClass("present");
-//     } else if (eleven < time) {
-//         $("#10am").removeClass("present").addClass("past");
-//     };
-
-    // if (time < eleven) {
-    //     $("#11am").addClass("future");
-    // } else if (eleven < time < twelve) {
-    //     $("#11am").removeClass("future").addClass("present");
-    // } else if (twelve < time) {
-    //     $("#11am").removeClass("present").addClass("past");
-    // };
-
-    // if (time < twelve) {
-    //     $("#12pm").addClass("future");
-    // } else if (twelve < time < one) {
-    //     $("#12pm").removeClass("future").addClass("present");
-    // } else if (one < time) {
-    //     $("#12pm").removeClass("present").addClass("past");
-    // };
-
-    // if (time < one) {
-    //     $("#1pm").addClass("future");
-    // } else if (one < time < two) {
-    //     $("#1pm").removeClass("future").addClass("present");
-    // } else if (two < time) {
-    //     $("#1pm").removeClass("present").addClass("past");
-    // };
-
-    // if (time < two) {
-    //     $("#2pm").addClass("future");
-    // } else if (two < time < three) {
-    //     $("#2pm").removeClass("future").addClass("present");
-    // } else if (three < time) {
-    //     $("#2pm").removeClass("present").addClass("past");
-    // };
-
-    // if (time < three) {
-    //     $("#3pm").addClass("future");
-    // } else if (three < time < four) {
-    //     $("#3pm").removeClass("future").addClass("present");
-    // } else if (four < time) {
-    //     $("#3pm").removeClass("present").addClass("past");
-    // };
-
-    // if (time < four) {
-    //     $("#4pm").addClass("future");
-    // } else if (four < time < five) {
-    //     $("#4pm").removeClass("future").addClass("present");
-    // } else if (five < time) {
-    //     $("#4pm").removeClass("present").addClass("past");
-    // };
-
-    // if (time < five) {
-    //     $("#5pm").addClass("future");
-    // } else if (five < time) {
-    //     $("#5pm").removeClass("future").addClass("present");
-    // } else if (six < time) {
-    //     $("#5pm").removeClass("present").addClass("past");
-    // };
-
-// };
 
 colorCode();
-
-
-
